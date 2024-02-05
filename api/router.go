0c8f971675d57dc73aad58bafb9d72e62c717d75
davlatabdullaev <davlatdavlat0206@gmail.com>
2024-02-05 21:41:51 +0500

    updated

--- api/router.go
+++ api/router.go
@@ -8,6 +8,7 @@ import (
 	"github.com/swaggo/gin-swagger" // gin-swagger middleware
     "github.com/swaggo/files" // swagger embed files
 )
+
 // @title           BAZAAR
 // @version         0.0.1
 // @description     An API for a store called BAZAAR
